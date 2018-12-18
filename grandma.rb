0e28037ec3febb775616d3033d537def91ab7e6d
Ivan Garcia <ivanvlora@gmail.com>
2018-12-18 17:04:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma phrase
-   if phrase
+   if phrase == phrase.downcase
      "HUH?! SPEAK UP, SONNY!"
    elsif phrase == phrase.upcase
      "NO, NOT SINCE 1938!"
