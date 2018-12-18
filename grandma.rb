edef78ef1015664945f5e0be07aefd710bca30a1
Ivan Garcia <ivanvlora@gmail.com>
2018-12-18 16:58:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma phrase
   "I LOVE YOU TOO PUMPKIN!"
     elsif phrase.upcase
     "NO, NOT SINCE 1938!"
-  else
+  elsif phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
